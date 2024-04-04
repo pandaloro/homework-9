02d8d043d48ad5e48a00002061369b9fe59d0583
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-04 16:10:18 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,8 +1,38 @@
 <?php
 
 namespace app\models;
+use app\core\Database;
+//using the database class namespace
+
 
 class Post
 {
-    //todo make methods here
+    //using the trait, bring in all of its methods
+    use Database;
+
+
+    public function getAllPosts() {
+        $query = "select * from posts";
+        return $this->fetchAll($query);
+    }
+
+    public function getPostById($id){
+        $query = "select * from posts where id = :id";
+        return $this->queryWithParams($query, ['id' => $id]);
+    }
+
+    public function savePost($inputData){
+        $query = "insert into posts (title, description) values (:title, :description);";
+        return $this->queryWithParams($query, $inputData);
+    }
+
+    public function updatePost($inputData){
+        $query = "update posts set title = :title, description = :description where id = :id";
+        return $this->queryWithParams($query, $inputData);
+    }
+
+    public function deletePost($inputData){
+        $query = "DELETE FROM posts where id = :id";
+        return $this->queryWithParams($query, $inputData);
+    }
 }
