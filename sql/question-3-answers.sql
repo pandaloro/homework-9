3eb20b274e686bdc4948132a0d1c49f95e299674
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-04 16:13:19 -0400

    Update question-3-answers.sql

--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,22 @@
 -- select by id 1:
-
+SELECT * FROM posts WHERE id = 1;
 
 -- select all posts where title = includes "title 2":
+SELECT * FROM posts WHERE title LIKE '%title 2%';
 
 
 -- select all posts and order by the title column alphabetically:
-
+SELECT * FROM posts ORDER BY title ASC;
 
 -- insert 3 new posts
-
+INSERT INTO posts (title, description) VALUES
+('New Post 1', 'Content for new post 1'),
+('New Post 2', 'Content for new post 2'),
+('New Post 3', 'Content for new post 3');
 
 -- update posts where id = 1, set the title to "new title"
-
+UPDATE posts SET title = 'New Title' WHERE id = 1;
 
 -- delete post where id = 2
+DELETE FROM posts WHERE id = 2;
+
